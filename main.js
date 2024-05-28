7cee4d97edb8301aafddc5c75196eea993c64dbd
simonTerminator <simonsvara@gmail.com>
2024-05-28 18:30:11 +0200

    GameLoop.js, bug fixing

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-import './style.css'
+import {resources} from "/src/Resource.js";
+import {Sprite} from "/src/Sprite.js";
+import {Vector2} from "/src/Vector2.js";
+import {GameLoop} from "/src/GameLoop.js";
+
+
+const canvas = document.querySelector("#game-canvas");
+const ctx = canvas.getContext("2d");
+
+const skySprite = new Sprite({
+    resource: resources.images.sky,
+    frameSize: new Vector2(320,180)
+})
+
+const groundSprite = new Sprite({
+    resource: resources.images.ground,
+    frameSize: new Vector2(320,180)
+})
+
+const shadow = new Sprite({
+    resource: resources.images.shadow,
+    frameSize: new Vector2(32,32),
+})
+
+const hero = new Sprite({
+    resource: resources.images.hero,
+    frameSize: new Vector2(32,32),
+    hFrames: 3,
+    vFrames: 8,
+    frame: 1
+})
+
+const heroPos = new Vector2(16 * 10, 16 * 5);
+
+const update = () => {
+    // Updating entities in the game
+    heroPos.x += 1;
+}
+
+const draw = () => {
+    skySprite.drawImage(ctx, 0, 0);
+    groundSprite.drawImage(ctx, 0,0);
+
+    // Centre the hero in the cell
+    const heroOffset = new Vector2(-8,-21);
+    const heroPosX = heroPos.x + heroOffset.x;
+    const heroPosY = heroPos.y + heroOffset.y;
+
+
+    shadow.drawImage(ctx, heroPosX, heroPosY);
+    hero.drawImage(ctx, heroPosX, heroPosY);
+}
+
+
+
+const gameLoop = new GameLoop(update,  draw);
+gameLoop.start();
---
+++ src/Resource.js
@@ -0,0 +1,29 @@
+class Resource{
+    constructor() {
+        // Everything we want to download
+        this.toLoad = {
+            sky: "/public/sprites/sky.png",
+            ground: "/public/sprites/ground.png",
+            hero: "/public/sprites/hero-sheet.png",
+            shadow: "/public/sprites/shadow.png",
+        };
+        // A bucket to keep all of our images
+        this.images = {};
+
+        // Load each image
+        Object.keys(this.toLoad).forEach(key => {
+            const img = new Image();
+            img.src = this.toLoad[key];
+            this.images[key] = {
+                image: img,
+                isLoaded: false
+            }
+            img.onload = () => {
+                this.images[key].isLoaded = true;
+            }
+        })
+    }
+}
+
+// Create one instance for the whole app to use
+export const resources = new Resource()

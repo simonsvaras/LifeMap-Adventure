0eb54b77a50d272179fa5727b4f682eba0f55fb2
simonTerminator <simonsvara@gmail.com>
2024-05-30 11:35:28 +0200

    Moving in the grid

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import {Sprite} from "/src/Sprite.js";
 import {Vector2} from "/src/Vector2.js";
 import {GameLoop} from "/src/GameLoop.js";
 import {DOWN, Input, LEFT, RIGHT, UP} from "/src/Input.js";
+import {gridCells} from "/src/helpers/grid.js";
+import {moveTowards} from "/src/helpers/moveToward.js";
 
 
 const canvas = document.querySelector("#game-canvas");
@@ -28,29 +30,58 @@ const hero = new Sprite({
     frameSize: new Vector2(32,32),
     hFrames: 3,
     vFrames: 8,
-    frame: 1
+    frame: 1,
+    position: new Vector2(gridCells(6), gridCells(5))
 })
 
-const heroPos = new Vector2(16 * 10, 16 * 5);
+// For grid movement
+const heroDestinationPosition = hero.position.duplicate();
+
 
 const input = new Input();
 
 const update = () => {
-    // Updating entities in the game
-    if(input.direction === DOWN){
-        heroPos.y += 1;
+
+    const distance = moveTowards(hero, heroDestinationPosition, 1) ;
+    const hasArrived = distance <= 1;
+    // Attempt to move again if the hero is at his position
+    if(hasArrived){
+        tryMove();
     }
-    if(input.direction === LEFT){
-        heroPos.x -= 1;
-    }
-    if(input.direction === UP){
-        heroPos.y -= 1;
-    }
-    if(input.direction === RIGHT){
-        heroPos.x += 1
+};
+
+const tryMove = () => {
+
+    if(!input.direction){
+        return;
     }
 
-};
+    let nextX = heroDestinationPosition.x;
+    let nextY = heroDestinationPosition.y;
+    const gridSize = 16;
+
+    // Updating entities in the game
+    if(input.direction === DOWN){
+        nextY += gridSize;
+        hero.frame = 0;
+    }
+    if(input.direction === LEFT){
+        nextX -= gridSize;
+        hero.frame = 9;
+    }
+    if(input.direction === UP){
+        nextY -= gridSize;
+        hero.frame = 6;
+    }
+    if(input.direction === RIGHT){
+        nextX += gridSize;
+        hero.frame = 3;
+    }
+
+    // TODO - check if that space is free
+    heroDestinationPosition.x = nextX;
+    heroDestinationPosition.y = nextY;
+}
 
 const draw = () => {
     skySprite.drawImage(ctx, 0, 0);
@@ -58,8 +89,8 @@ const draw = () => {
 
     // Centre the hero in the cell
     const heroOffset = new Vector2(-8,-21);
-    const heroPosX = heroPos.x + heroOffset.x;
-    const heroPosY = heroPos.y + heroOffset.y;
+    const heroPosX = hero.position.x + heroOffset.x;
+    const heroPosY = hero.position.y + heroOffset.y;
 
 
     shadow.drawImage(ctx, heroPosX, heroPosY);

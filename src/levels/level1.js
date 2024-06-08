94b5a9f4bd79ec5992b67b5255d4ddbbebb75b93
simonTerminator <simonsvara@gmail.com>
2024-06-08 15:07:19 +0200

    Loading resources repair

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -52,7 +52,7 @@ const addVerticalWall = (x, startY, endY, step = 16) => {
 /**
  * Fetches wall coordinates from a JSON file and adds them to the walls set.
  */
-fetch('../../LifeMap-Adventure/public/walls/mapV2.json')
+fetch('../../LifeMap-Adventure/public/walls/home.json')
     .then(response => response.json())
     .then(data => {
         data.walls.forEach(wall => {
